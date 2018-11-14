d4b6020e020fc9d04d978f671e873964604803aa
Alin <alinstud7@gmail.com>
2018-11-15 00:31:02 +0200

    Student Profile updated

--- app/src/main/java/com/example/adrianantonescu/qa/StudentProfileActivity.java
+++ app/src/main/java/com/example/adrianantonescu/qa/StudentProfileActivity.java
@@ -1,13 +1,44 @@
 package com.example.adrianantonescu.qa;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class StudentProfileActivity extends AppCompatActivity {
-
+    ImageView imgSettings;
+    Button btnBack;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_profile);
+        init();
+    }
+    private void init()
+    {
+        imgSettings=findViewById(R.id.student_activity_image_view_settings);
+        imgSettings.setOnClickListener(openSettings());
+        btnBack=findViewById(R.id.student_profile_btn_back);
+        btnBack.setOnClickListener(backTo());
+    }
+    private View.OnClickListener openSettings()
+    {
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(getApplicationContext(),StudentProfileSettingsActivity.class);
+                startActivity(intent);
+            }
+        };
+    }
+    private View.OnClickListener backTo(){
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        };
     }
 }
--- app/src/main/java/com/example/adrianantonescu/qa/TeacherProfileActivity.java
+++ app/src/main/java/com/example/adrianantonescu/qa/TeacherProfileActivity.java
@@ -1,13 +1,33 @@
 package com.example.adrianantonescu.qa;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class TeacherProfileActivity extends AppCompatActivity {
 
+    ImageView ivSettings;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_teacher_profile);
+        initializare();
+    }
+
+    private void initializare() {
+        ivSettings=findViewById(R.id.teacher_activity_image_view_settings);
+        ivSettings.setOnClickListener(openSettings());
+    }
+    private View.OnClickListener openSettings()
+    {
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(getApplicationContext(),TeacherProfileSettingsActivity.class);
+                startActivity(intent);
+            }
+        };
     }
 }
