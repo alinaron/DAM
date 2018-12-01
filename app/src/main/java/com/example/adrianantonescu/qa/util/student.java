c65f03d97b692c920504141bd2ccfe8114e4f813
Antonescu Adrian-Gabriel <33223003+adrianantonescu@users.noreply.github.com>
2018-12-01 21:46:50 +0200

    cls

--- app/src/main/java/com/example/adrianantonescu/qa/util/student.java
+++ app/src/main/java/com/example/adrianantonescu/qa/util/student.java
@@ -1,17 +1,52 @@
 package com.example.adrianantonescu.qa.util;
 
 public class student extends user {
+    private String nume;
+    private String prenume;
     private String grupa;
     private String serie;
     private int an;
     private String specializare;
+    private String email;
 
-    public student(String nume, String prenume, String username, String password, String grupa, String serie, int an, String specializare) {
-        super(nume, prenume, username, password);
+    public student(String username, String password, String nume, String prenume, String grupa, String serie, int an, String specializare, String email) {
+        super(username, password);
+        this.nume = nume;
+        this.prenume = prenume;
         this.grupa = grupa;
         this.serie = serie;
         this.an = an;
         this.specializare = specializare;
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "student{" +
+                "nume='" + nume + '\'' +
+                ", prenume='" + prenume + '\'' +
+                ", grupa='" + grupa + '\'' +
+                ", serie='" + serie + '\'' +
+                ", an=" + an +
+                ", specializare='" + specializare + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+
+    public String getNume() {
+        return nume;
+    }
+
+    public void setNume(String nume) {
+        this.nume = nume;
+    }
+
+    public String getPrenume() {
+        return prenume;
+    }
+
+    public void setPrenume(String prenume) {
+        this.prenume = prenume;
     }
 
     public String getGrupa() {
@@ -46,13 +81,11 @@ public class student extends user {
         this.specializare = specializare;
     }
 
-    @Override
-    public String toString() {
-        return "student{" +
-                "grupa='" + grupa + '\'' +
-                ", serie='" + serie + '\'' +
-                ", an=" + an +
-                ", specializare='" + specializare + '\'' +
-                '}';
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 }
--- app/src/main/java/com/example/adrianantonescu/qa/util/teacher.java
+++ app/src/main/java/com/example/adrianantonescu/qa/util/teacher.java
@@ -3,11 +3,33 @@ package com.example.adrianantonescu.qa.util;
 import java.util.Arrays;
 
 public class teacher extends user {
+    private String nume;
+    private String prenume;
     private String[] materii;
+    private String email;
 
-    public teacher(String nume, String prenume, String username, String password, String[] materii) {
-        super(nume, prenume, username, password);
+    public teacher(String username, String password, String nume, String prenume, String[] materii, String email) {
+        super(username, password);
+        this.nume = nume;
+        this.prenume = prenume;
         this.materii = materii;
+        this.email = email;
+    }
+
+    public String getNume() {
+        return nume;
+    }
+
+    public void setNume(String nume) {
+        this.nume = nume;
+    }
+
+    public String getPrenume() {
+        return prenume;
+    }
+
+    public void setPrenume(String prenume) {
+        this.prenume = prenume;
     }
 
     public String[] getMaterii() {
@@ -18,10 +40,21 @@ public class teacher extends user {
         this.materii = materii;
     }
 
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
     @Override
     public String toString() {
         return "teacher{" +
-                "materii=" + Arrays.toString(materii) +
+                "nume='" + nume + '\'' +
+                ", prenume='" + prenume + '\'' +
+                ", materii=" + Arrays.toString(materii) +
+                ", email='" + email + '\'' +
                 '}';
     }
 }
